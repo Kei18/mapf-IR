da2d16a11c52b519c1c92f44f3ffc690e63b7952
Kei18 <keisuke.oku18@gmail.com>
2021-01-27 12:51:03 +0900

    add exp scripts

---
+++ exp_scripts/exp_init-inefficient_refine_random-32-32-20.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+set -e
+
+scen_start=1
+scen_end=25
+force=0
+
+map="random-32-32-20.map"
+agents_list="110"
+well_formed=0
+
+solvers=(
+    "IR_SINGLE_PATHS"
+    "IR_FIX_AT_GOALS"
+    "IR -S 10"
+    "IR -S 30"
+    "IR_FOCUS_GOALS"
+    "IR_MDD"
+    "IR_BOTTLENECK"
+    "IR_HYBRID")
+
+refine_limit=500
+refine_cnt=10000000
+comp_time_limit=30000
+
+for solver in "${solvers[@]}"
+do
+    sh `dirname $0`/run.sh \
+       $map \
+       "$agents_list" \
+       "$solver -n ${refine_cnt} -t ${refine_limit} -T ${comp_time_limit}" \
+       $scen_start \
+       $scen_end \
+       $well_formed \
+       $force
+done
