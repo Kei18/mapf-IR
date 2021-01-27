707bfc2b554258eb8446499591692649dc4d9dee
Kei18 <keisuke.oku18@gmail.com>
2021-01-27 12:11:57 +0900

    add instances

--- exp_scripts/run.sh
+++ exp_scripts/run.sh
@@ -6,17 +6,22 @@ agents_list=$2
 solver=$3
 scen_start=$4
 scen_end=$5
+well_formed=$6
+force=$7
 
 PROJECT_DIR=`dirname $0`/..
 map_trimed=${map%.map}
 
 ## check git status
-GIT_STATUS_RESULT=`git status -s`
-if [ ${#GIT_STATUS_RESULT} -ne 0 ]
+if [ ${force} -ne 1 ]
 then
-    echo "Untracked changes exist. Commit them beforehand."
-    git status -s
-    exit 1
+   GIT_STATUS_RESULT=`git status -s`
+   if [ ${#GIT_STATUS_RESULT} -ne 0 ]
+   then
+       echo "Untracked changes exist. Commit them beforehand."
+       git status -s
+       exit 1
+   fi
 fi
 GIT_RECENT_COMMIT=`git log -1 --pretty=format:"%H"`
 
@@ -45,6 +50,10 @@ do
     while [ $scen -lt `expr $scen_end + 1` ]
     do
         scen_file="${map_trimed}_${agent_num}agents_${scen}.txt"
+        if [ ${well_formed} -eq 1 ]
+        then
+            scen_file="${map_trimed}_${agent_num}agents_well-formed_${scen}.txt"
+        fi
         $PROJECT_DIR/build/app \
                     -i $PROJECT_DIR/instances/$scen_file \
                     -o $OUTPUT_DIR/$scen_file \
